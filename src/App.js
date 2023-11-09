9334634d4dc3ad28091e0b8963519cc119f17983
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-09 13:01:08 +0330

    signing with email and password and signing in with created account added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Login from './components/Login';
+import LandingPage from './components/LandingPage';
+
+// SPA
+import { Routes , Route , Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <Routes>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/*' element={<Navigate to={"/login"} />} />
+      <Route path='/landingpage' element={<LandingPage />} />
+    </Routes>
   );
 }   
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,89 @@
-import React from 'react';
-import { GoogleAuthProvider,signInWithPopup } from 'firebase/auth';
-import { auth } from '../firebase/firebase';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import styles from "./Login.module.css";
 
+// firebase
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase";
 
 const Login = () => {
-    const googleHandler =(e) => {
-        e.preventDefault();
-        const provider =new GoogleAuthProvider();
-        return signInWithPopup(auth,provider)
-        
-    }
-    return (
-        <div>
-            <button onClick={googleHandler}>Sign In</button>
-        </div>
-    );
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const inputHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const googleHandler = (e) => {
+    e.preventDefault();
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+  const navigate = useNavigate();
+  const signupHandler = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredintial) => {
+        const user = userCredintial.user;
+        console.log(user);
+        alert("Successfully Joined");
+        setData({ email: "", password: "" });
+        navigate("/landingpage");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+  const signinHandler = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredintial) => {
+        const user = userCredintial.user;
+        console.log(user);
+        alert("Successfully Joined");
+        setData({ email: "", password: "" });
+        navigate("/landingpage");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  return (
+    <>
+      <h1>Welcome To The Mini Shop</h1>
+      <form className={styles.formcontainer}>
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          name="email"
+          value={data.email}
+          onChange={inputHandler}
+        />
+        <input
+          type="password"
+          placeholder="Enter Your Password"
+          name="password"
+          value={data.password}
+          onChange={inputHandler}
+        />
+        <button onClick={signupHandler}>Sign Up</button>
+      </form>
+      <h4> Sign In With Google Account </h4>
+      <button onClick={googleHandler}>
+        <FcGoogle />
+      </button>
+      <h2>Already have Account??</h2>
+      <button onClick={signinHandler}>Sign In</button>
+    </>
+  );
 };
 
 export default Login;
