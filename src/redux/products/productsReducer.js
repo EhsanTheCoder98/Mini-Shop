1e3e10aa404378e048e2c5f7c570614990dcc0fb
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-19 18:10:12 +0330

    productsReducer Done!

--- src/redux/products/productsReducer.js
+++ src/redux/products/productsReducer.js
@@ -6,6 +6,25 @@ const initialState = {
 
 const productsReducer = (state=initialState,action) => {
     switch(action.type) {
-        case 
+        case "FETCH-PRODUCTS":
+            return{
+                ...state,
+                loading:true
+            }
+        case "FETCH-PRODUCTS-SUCCESS":
+            return{
+                ...state,
+                loading:false,
+                products:action.payload
+            }    
+        case "FETCH-PRODUCTS-FAILURE":
+            return {
+                ...state,
+                loading:false,
+                products:[],
+                error:action.payload
+            }    
     }
 }
+
+export default productsReducer;
