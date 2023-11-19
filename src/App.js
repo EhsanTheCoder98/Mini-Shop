6b2b4b4d0ee4dae6859bc0bf8ef246376d19f565
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-19 19:23:49 +0330

    Navbar component Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
 
 // redux
 import { Provider } from "react-redux";
@@ -11,10 +12,11 @@ import { Routes, Route, Navigate } from "react-router-dom";
 const App = () => {
   return (
     <Provider store={store}>
+      <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<Navigate to={"/login"} />} />
-        <Route path="/landingpage" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Navigate to={"/login"} />} />
+          <Route path="/landingpage" element={<LandingPage />} />
       </Routes>
     </Provider>
   );
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,24 @@
-import React , {useEffect} from 'react';
+import React, { useEffect } from "react";
 
 // redux
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { useDispatch } from 'react-redux';
-import { fetchAPI } from '../redux/products/productsAction';
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useDispatch } from "react-redux";
+import { fetchAPI } from "../redux/products/productsAction";
 
 const LandingPage = () => {
-    const dispatch = useDispatch();
-    const productsData = useSelector(state => state.productsContainer);
+  const dispatch = useDispatch();
+  const productsData = useSelector((state) => state.productsContainer);
 
-    useEffect(()=>{
-        dispatch(fetchAPI());
-    },[])
-    return (
-        <div>
-            {productsData.products.map(item=><p>{item.title}</p>)}
-        </div>
-    );
+  useEffect(() => {
+    dispatch(fetchAPI());
+  }, []);
+  return (
+    <div>
+      {productsData.products.map((item) => (
+        <p>{item.title}</p>
+      ))}
+    </div>
+  );
 };
 
 export default LandingPage;
