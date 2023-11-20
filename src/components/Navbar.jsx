d4c5c1a1441e94a518c279fa9213254e5dce5651
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-20 18:54:25 +0330

    Finalize initial styling for Navbar component prototype

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { CgProfile } from "react-icons/cg";
+import { FaShoppingCart } from "react-icons/fa";
+import { PiSignOutLight } from "react-icons/pi";
+import styles from "./Navbar.module.css"
 
 const Navbar = () => {
     return (
-        <div>
-            NAVBAR
+        <div className={styles.container}>
+            <button><CgProfile className={styles.profileLogo} /></button>
+            <button><FaShoppingCart className={styles.cartLogo} /></button>
+            <h3>Mini Shop</h3>
+            <button className={styles.signoutButton}><PiSignOutLight className={styles.signoutLogo} /></button>
         </div>
     );
 };
