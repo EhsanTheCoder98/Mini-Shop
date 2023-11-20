a219836a23c3ba60ee6c030d8a1a55c9af195efc
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-20 19:01:13 +0330

    Implement sign-out functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { CgProfile } from "react-icons/cg";
 import { FaShoppingCart } from "react-icons/fa";
 import { PiSignOutLight } from "react-icons/pi";
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
+import { useNavigate } from 'react-router-dom';
+
+// firebase
+import { getAuth , signOut } from 'firebase/auth';
+
 
 const Navbar = () => {
+    const auth = getAuth();
+    const navigate = useNavigate();
     return (
         <div className={styles.container}>
             <button><CgProfile className={styles.profileLogo} /></button>
             <button><FaShoppingCart className={styles.cartLogo} /></button>
             <h3>Mini Shop</h3>
-            <button className={styles.signoutButton}><PiSignOutLight className={styles.signoutLogo} /></button>
+            <button className={styles.signoutButton} onClick={()=>signOut(auth).then(()=>{
+                alert("SignedOut Successfully");
+                navigate("/login")
+            })}><PiSignOutLight className={styles.signoutLogo} /></button>
         </div>
     );
 };
