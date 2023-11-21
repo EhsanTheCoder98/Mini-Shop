86308578132de07bf390b1c5ce8ee0449c526382
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-21 23:38:59 +0330

    Loading and error handled

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import styles from './LandingPage.module.css';
+import styles from "./LandingPage.module.css";
 
 // components
 import ProductsDiv from "./ProductsDiv";
@@ -15,11 +15,19 @@ const LandingPage = () => {
 
   useEffect(() => {
     dispatch(fetchAPI());
-    console.log("rendered")
+    console.log("rendered");
   }, []);
   return (
     <div className={styles.container}>
-      {productsData.products.map(item=><ProductsDiv key={item.id} products={item} />)}
+      {productsData.loading ? (
+        <h1 className={styles.phrases}>LOADING PRODUCTS</h1>
+      ) : productsData.error ? (
+        <h1 className={styles.phrases}>There is a problem!</h1>
+      ) : (
+        productsData.products.map((item) => (
+          <ProductsDiv key={item.id} products={item} />
+        ))
+      )}
     </div>
   );
 };
