d0d9832eade964724ae553574b66076450be516d
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-29 18:07:33 +0330

    filtering products added!

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -14,13 +14,22 @@ const LandingPage = () => {
   const dispatch = useDispatch();
   const [isVisible,setIsVisibile] = useState(false);
   const productsData = useSelector((state) => state.productsContainer);
-
   useEffect(() => {
     dispatch(fetchAPI());
     setTimeout(()=>{
       setIsVisibile(true);
     },1200)
   }, []);
+  const [sorted , setSorted] = useState(productsData.products);
+  const sortProducts = (category) => {
+    if(productsData.products){
+      const sort = productsData.products.filter(product => product.category === category);
+      setSorted(sort);
+    }
+  }
+  const resetStore = () => {
+    setSorted(productsData.products)
+  }
   return (
     <div className={styles.bigContainer}>
       <div className={`${styles.leftcontainer} ${isVisible ? styles.fade : ''}`}>
@@ -32,13 +41,14 @@ const LandingPage = () => {
         ) : productsData.error ? (
           <h1 className={styles.phrases}>{productsData.error}</h1>
         ) : (
-          productsData.products.map((item) => (
+          sorted.map((item) => (
             <ProductsDiv key={item.id} products={item} />
           ))
         )}
       </div>
       {!productsData.loading && !productsData.error ? <div className={`${styles.rightcontainer} ${isVisible ? styles.fade : ''}`}>
-        bonus status filters
+        <button onClick={()=>sortProducts("men's clothing")}>Mens clothing</button>
+        <button onClick={()=>resetStore()}>Reset</button>
       </div> : null }
       
     </div>
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import styles from './ProductDetails.module.css';
+import React from "react";
+import styles from "./ProductDetails.module.css";
 
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 // redux
-import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 const ProductDetails = () => {
-    const params = useParams();
-    const products = useSelector(state=>state.productsContainer.products);
-    const data = products[params.id - 1];
-    const {title} = data;
+  const params = useParams();
+  const products = useSelector((state) => state.productsContainer.products);
+  const data = products[params.id - 1];
+  const { title } = data;
 
-    return (
-        <div className={styles.container}>
-          <p style={{padding:"60px"}}>{title}</p>
-        </div>
-    );
+  return (
+    <div className={styles.bigcontainer}>
+      <div className={styles.container}>
+        <p>{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
