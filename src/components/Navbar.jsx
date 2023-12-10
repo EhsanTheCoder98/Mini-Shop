ba50f4cc4674864aed6cf894c05e2af85e53b566
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-12-10 19:38:18 +0330

    overallProducts logic done and shown in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,20 @@ import { useNavigate , Link } from 'react-router-dom';
 // firebase
 import { getAuth , signOut } from 'firebase/auth';
 
+// redux
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
 
 const Navbar = () => {
     const auth = getAuth();
     const navigate = useNavigate();
+    const overall = useSelector(state=>state.cartContainer.overallProducts)
     return (
         <div className={styles.container}>
             <Link to="profile"><CgProfile className={styles.profileLogo} /></Link>
+            <div className={styles.cartLogoContainer}>
             <Link to="cart"><FaShoppingCart className={styles.cartLogo} /></Link>
+            <span>{overall}</span>
+            </div>
             <Link to="/landingpage" className={styles.logo}>Mini Shop</Link>
             <button className={styles.signoutButton} onClick={()=>signOut(auth).then(()=>{
                 alert("SignedOut Successfully");
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -5,6 +5,11 @@ const initialState = {
     overallPrice:0
 }
 
+const collector = (item) => {
+    const overallProducts = item.reduce((total,product)=>total+product.quantity,0);
+    return{overallProducts:overallProducts}
+}
+
 const cartReducer = (state=initialState,action) => {
     console.log(state.selectedProducts);
     switch(action.type) {
@@ -17,28 +22,32 @@ const cartReducer = (state=initialState,action) => {
             }
             return{
                 ...state,
-                selectedProducts:[...state.selectedProducts]
+                selectedProducts:[...state.selectedProducts],
+                ...collector(state.selectedProducts)
             }
             case "Delete" :
                 const newSelectedProducts = state.selectedProducts.filter(item => item.id !== action.payload.id);
                 state.selectedProducts = newSelectedProducts;
                 return {
                     ...state,
-                    selectedProducts:[...state.selectedProducts]
+                    selectedProducts:[...state.selectedProducts],
+                ...collector(state.selectedProducts)
                 } 
             case "Increase":
                 const Iindex = state.selectedProducts.findIndex(item=>item.id === action.payload.id);
                 state.selectedProducts[Iindex].quantity++
                 return{
                     ...state,
-                    selectedProducts:[...state.selectedProducts]
+                    selectedProducts:[...state.selectedProducts],
+                ...collector(state.selectedProducts)
                 }
             case "Decrease":
                 const Dindex = state.selectedProducts.findIndex(item=>item.id === action.payload.id);
                 state.selectedProducts[Dindex].quantity--
                 return{
                     ...state,
-                    selectedProducts:[...state.selectedProducts]
+                    selectedProducts:[...state.selectedProducts],
+                ...collector(state.selectedProducts)
                 }    
                 default: return state    
             }
