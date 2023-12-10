4b295475c1c6107cd93a2e2ec6599e04c7674a9e
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-12-10 13:49:15 +0330

    Like feature ui added

--- src/components/ProductsDiv.jsx
+++ src/components/ProductsDiv.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React , {useState} from 'react';
 import styles from './ProductsDiv.module.css';
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FaHeart } from "react-icons/fa";
 
 // methods
 import { titleHandler } from '../methods/functions';
 
 const ProductsDiv = ({products}) => {
+    const[like,setLike] = useState(false);
+    const likeHandler = () => {
+        setLike(!like)
+    }
     return (
         <div className={styles.container}>
             <img src={products.image}/>
             <p className={styles.title}>{titleHandler(products.title)}</p>
             <span className={styles.title}>{products.price}$</span>
             <Link to={`productdetail/${products.id}`} className={styles.detail}>Details&Buy</Link>
+            <button onClick={likeHandler}><FaHeart className={like?styles.likedIcon:styles.likeIcon} /></button>
         </div>
     );
 };
+
 
 export default ProductsDiv;
