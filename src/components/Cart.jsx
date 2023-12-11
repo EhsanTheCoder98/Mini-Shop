a184ab69f821b63e52962dcabd529d3b2e50fe6c
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-12-11 15:10:43 +0330

    selected products shown in the cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Cart.module.css';
 
+// components
+import CartProducts from './CartProducts';
+
+// redux
+import { useSelector , useDispatch } from 'react-redux';
+
 const Cart = () => {
+    const selectedProducts = useSelector(state=>state.cartContainer.selectedProducts);
     return (
         <div className={styles.container}>
-            CART    
+            {selectedProducts.map(item=><CartProducts key={item.id} products={item}/>)}
         </div>
     );
 };
